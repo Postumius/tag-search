b98e0db16245317cb2ebf571267b36acc172f4d2
Noah Ma <noahfierz@gmail.com>
2020-05-05 23:26:15 -0700

    changed build-the-list to use regexes, wrote boolean operations for tags

--- tag-search.rkt
+++ tag-search.rkt
@@ -1,24 +1,51 @@
 #lang racket
 
-(require (for-syntax syntax/parse))
+(require (for-syntax syntax/parse) racket/function)
 
 (struct entry (title tags)
   #:transparent)
 
-(define-syntax (build-the-list stx)
-  (syntax-parse stx
-    [(build-the-list
-      (title tag ...)
-      ...)
-     #'(list
-        (entry
-         (symbol->string
-          (syntax->datum #'title))
-         (list (symbol->string
-                (syntax->datum #'tag)) ...))
-        ...)]))
+(define (build-the-list str)
+  (define (split-remove-empty pat str)
+    (filter
+     (λ(s) (not (equal? "" s)))
+     (regexp-split pat str)))
+  [define split
+    (map (curry split-remove-empty #rx"\n+")
+         (split-remove-empty #rx"~+" str))]
+  (map (match-lambda
+         [(cons title tags) (entry title tags)])
+       split))
 
-(define the-list
-    (build-the-list
-     (game1 tag1 tag2)
-     (game2 tag2 tag-with-long-name)))
+(define (f-and a b) (and a b))
+(define (f-or a b) (or a b))
+
+(define (compose-preds op p1 p2)
+  (λ(val) (op (p1 val) (p2 val))))
+
+(define (AND . tags)
+  (match tags
+    ['() (λ(st) #t)]
+    [(cons t ts)
+     (if (procedure? t)
+         (compose-preds f-and t (apply AND ts))
+         (compose-preds
+          f-and (curryr set-member? t) (apply AND ts)))]))
+
+(define (OR . tags)
+  (match tags
+    ['() (λ(st) #f)]
+    [(cons t ts)
+     (if (procedure? t)
+         (compose-preds f-or t (apply OR ts))
+         (compose-preds
+          f-or (curryr set-member? t) (apply OR ts)))]))
+
+(define (NOT tag)
+  (if (procedure? tag)
+      (compose not tag)
+      (compose not (curryr set-member? tag))))
+
+
+
+(define st (set "1" "2" "3"))
